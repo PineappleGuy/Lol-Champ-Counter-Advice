8a4aa57d9a7bd26249a424a1d55a3153a4739c94
Garth Lilly <garff247@gmail.com>
2020-04-27 16:53:08 -0700

    added fetch for champions to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,7 @@
 const BASE_URL = "http://localhost:3000"
 const CHAMPIONS_URL = `${BASE_URL}/champions`
 const COMMENTS_URL = `${BASE_URL}/comments`
+
+fetch(CHAMPIONS_URL)
+    .then(resp => resp.json())
+    .then(json => console.log(json));
