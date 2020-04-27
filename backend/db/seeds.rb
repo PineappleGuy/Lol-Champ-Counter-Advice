3af40eb6b47c964d9a459dc63d4e451c5f7bbd97
Garth Lilly <garff247@gmail.com>
2020-04-27 15:16:33 -0700

    added anivia-asol'

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -11,3 +11,8 @@ ahri = Champion.create(name: "Ahri", image_url: "https://images.contentstack.io/
 akali = Champion.create(name: "Akali", image_url: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt80ff58fe809777ff/5db05fa8adc8656c7d24e7d6/RiotX_ChampionList_akali.jpg?quality=90&width=250")
 alistar = Champion.create(name: "Alistar", image_url: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt3b366925d2fd8fdb/5db05fa856458c6b3fc1750b/RiotX_ChampionList_alistar.jpg?quality=90&width=250")
 amumu = Champion.create(name: "Amumu", image_url: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250")
+anivia = Champion.create(name: "Anivia", image_url: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt3d24a1482453088a/5db05fa8df78486c826dcce8/RiotX_ChampionList_anivia.jpg?quality=90&width=250")
+annie = Champion.create(name: "Annie", image_url: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt28c708665427aef6/5db05fa89481396d6bdd01a6/RiotX_ChampionList_annie.jpg?quality=90&width=250")
+aphelios = Champion.create(name: "Aphelios", image_url: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aphelios_0.jpg")
+ashe = Champion.create(name: "Ashe", image_url: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt943aae038e2dee98/5db05fa8e9effa6ba5295f91/RiotX_ChampionList_ashe.jpg?quality=90&width=250")
+asol = Champion.create(name: "Aurelion Sol", image_url: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt5dd3569fc67d6664/5db05fa8bd24496c390ae4d8/RiotX_ChampionList_aurelionsol.jpg?quality=90&width=250")
