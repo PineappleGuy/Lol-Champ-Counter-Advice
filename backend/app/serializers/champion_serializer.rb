8bd6feb21b7198d66870d4dfae3a4ff2681344f4
Garth Lilly <garff247@gmail.com>
2020-05-07 12:52:53 -0700

    deleted serializers that werent being used

--- backend/app/serializers/champion_serializer.rb
+++
@@ -1,6 +0,0 @@
-class ChampionSerializer
-    include FastJsonapi::ObjectSerializer
-    attributes :name, :image_url
-    has_many :comments
-
-end
--- backend/app/serializers/comment_serializer.rb
+++
@@ -1,6 +0,0 @@
-class CommentSerializer
-    include FastJsonapi::ObjectSerializer
-    attributes :content, :upvotes, :downvotes, :champion_id
-    belongs_to :champion
-
-end
